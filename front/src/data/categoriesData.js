bf7827a83acb00b04ba81a24ea8f2b62c16d020e
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-04-11 01:06:56 +0200

    Create categoriesData

---
+++ front/src/data/categoriesData.js
@@ -0,0 +1,21 @@
+import img1 from '../assets/categoriesImgs/categories-1.jpeg';
+import img2 from '../assets/categoriesImgs/categories-2.jpeg';
+import img3 from '../assets/categoriesImgs/categories-3.jpeg';
+
+export const categories = [
+    {
+        id: 1,
+        img: img1,
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: img2,
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: img3,
+        title: "LIGHT JACKETS",
+    },
+];
