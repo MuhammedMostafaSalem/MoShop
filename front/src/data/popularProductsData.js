66d0ba74b908a718e2a6f8ee2f8d5cbe35c9bfb3
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-04-18 12:09:36 +0200

    Create popularProductsData

---
+++ front/src/data/popularProductsData.js
@@ -0,0 +1,43 @@
+import img1 from '../assets/popularProductsImgs/1.png';
+import img2 from '../assets/popularProductsImgs/2.png';
+import img3 from '../assets/popularProductsImgs/3.png';
+import img4 from '../assets/popularProductsImgs/4.png';
+import img5 from '../assets/popularProductsImgs/5.png';
+import img6 from '../assets/popularProductsImgs/6.png';
+import img7 from '../assets/popularProductsImgs/7.webp';
+import img8 from '../assets/popularProductsImgs/8.png';
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: img1,
+    },
+    {
+        id: 2,
+        img: img2,
+    },
+    {
+        id: 3,
+        img: img3,
+    },
+    {
+        id: 4,
+        img: img4,
+    },
+    {
+        id: 5,
+        img: img5,
+    },
+    {
+        id: 6,
+        img: img6,
+    },
+    {
+        id: 7,
+        img: img7,
+    },
+    {
+        id: 8,
+        img: img8,
+    },
+]
