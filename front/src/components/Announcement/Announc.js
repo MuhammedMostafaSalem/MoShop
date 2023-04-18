fcba9c54805a798299df9a778eeba8ed0e2c6d07
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-04-18 23:56:00 +0200

    create announcement component

---
+++ front/src/components/Announcement/Announc.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Announc.css'
+
+const Announc = () => {
+    return (
+        <div className='announcement'>Super Deal! Free Shipping on Orders Over $50</div>
+    )
+}
+
+export default Announc
