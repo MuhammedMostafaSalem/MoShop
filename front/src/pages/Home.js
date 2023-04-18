72d4f6f3d5296222d10c24a015b30ec891ab7b27
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-04-18 12:19:38 +0200

    Add Products in Home page

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Slider from '../components/Slider/Slider'
 import Categories from '../components/Categories/Categories';
+import Products from '../components/Products/Products';
 
 const Home = () => {
     return (
         <div>
             <Slider/>
             <Categories/>
+            <Products/>
         </div>
     )
 }
