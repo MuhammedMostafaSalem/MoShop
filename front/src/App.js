31d2820a14b3989d44758fcce0b2a9761d739146
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-04-18 11:08:18 +0200

    Create react-router-dom in app.js file

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NavBar from './components/utility/Navbar';
+
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route index element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
