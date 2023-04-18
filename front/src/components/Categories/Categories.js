6b4035a43a9d1b1645eb53623b35a97f21f45430
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-04-18 10:52:55 +0200

    Update in categories components

--- front/src/components/Categories/Categories.js
+++ front/src/components/Categories/Categories.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import './Categories.css'
-import { Container } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
 import CategoryItem from '../categoryItem/CategoryItem'
 import { categories } from '../../data/categoriesData';
 
 const Categories = () => {
     return (
         <Container className='divContainer'>
-        {
-            categories.map(
-                (item) => (
-                    <CategoryItem item={item} key={item.id} />
-                )
-            )
-        }
+            <Row>
+                {
+                    categories.map(
+                        (item) => (
+                            <CategoryItem item={item} key={item.id} />
+                        )
+                    )
+                }
+            </Row>
         </Container>
     )
 }
--- front/src/components/categoryItem/CategoryItem.js
+++ front/src/components/categoryItem/CategoryItem.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import './CategoryItem.css'
+import { Col } from 'react-bootstrap'
 const CategoryItem = ({item}) => {
     return (
-        <div className='categoryItem'>
-            <img src={item.img} alt='' />
-            <div className='info'>
-                <h3>{item.title}</h3>
-                <div className='btn'>SHOP NOW</div>
+        <Col lg='4' md='6' sm='6' xs='12'>
+            <div className='categoryItem'>
+                <img src={item.img} alt='' />
+                <div className='info'>
+                    <h3>{item.title}</h3>
+                    <div className='btn'>SHOP NOW</div>
+                </div>
             </div>
-        </div>
+        </Col>
     )
 }
 
